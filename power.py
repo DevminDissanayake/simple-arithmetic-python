cdf32f1a4ee507a953390b76007fb3d6017ddda5
Devmin Dissanayake <dissanayake.devmin@gmail.com>
2021-12-25 20:37:07 -0500

    Program created

---
+++ power.py
@@ -0,0 +1,10 @@
+import math
+
+x =  int(input("Enter number x:"))
+y =  int(input("Enter number y:"))
+
+exponent = x ** y
+print("x**y = " , exponent)
+
+log = math.log2(x)
+print("log(x) = ", log)
